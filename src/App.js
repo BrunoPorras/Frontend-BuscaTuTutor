f6220ceb0f47952e1f7d02e1279de26358ea1144
BrunoPorras <alefran2020@gmail.com>
2022-06-01 17:05:45 -0500

    Contenedor de rutas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-//  Páginas
+//  Páginas base
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
