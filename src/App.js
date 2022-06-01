8a847999bb693f62ea5474013391cab1bfa173e4
BrunoPorras <alefran2020@gmail.com>
2022-06-01 17:05:01 -0500

    Páginas base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+//  Páginas
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+
 const App = () => {
+
     return (
-        <div className="App">
-            Tanque cragg de craggs
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="" element={<Home/>}/>
+                <Route path="login" element={<Login/>}/>
+                <Route path="register" element={<Register/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
