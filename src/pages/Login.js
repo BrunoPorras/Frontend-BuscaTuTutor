e92e994ffc86bfe770ee4fa8b4bdcdc173d82e4f
BrunoPorras <alefran2020@gmail.com>
2022-06-01 17:06:43 -0500

    Páginas base

---
+++ src/pages/Login.js
@@ -0,0 +1,8 @@
+
+const Login = () => {
+    return (
+        <div>Login page</div>
+    )
+}
+
+export default Login
---
+++ src/pages/NotFound.js
@@ -0,0 +1,8 @@
+
+const NotFound = () => {
+    return (
+        <div>Esta ruta no es válida</div>
+    )
+}
+
+export default NotFound
