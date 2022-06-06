25847ba852989ef609d21ae4ffa602a0db29a34e
BrunoPorras <alefran2020@gmail.com>
2022-06-06 18:54:00 -0500

    Navbar de landing page y demo

--- src/components/Home/NavbarLanding.js
+++ src/components/Home/NavbarLanding.js
@@ -1,6 +1,9 @@
 //  React
 import { Link } from 'react-router-dom'
 
+//  Hooks
+import { useMenuMobile } from '../../hooks/useMenuMobile'
+
 //  Estilos, assets y efectos
 import { Link as ScrollLink } from 'react-scroll'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,13 +11,9 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import icono from '../../assets/Landing page/icono.png'
 import styles from '../../styles/Navbar.module.css'
 
-import { useMenuMobile } from '../../hooks/useMenuMobile'
-
-
-const NavbarLanding = () => {
+const NavbarLanding = ({ demoMode = false }) => {
 
     const [menuOpen, changeMenuOpen, closeMenuOpen ] = useMenuMobile()
-
 
     return(
         <div className={styles.container}>
@@ -29,22 +28,37 @@ const NavbarLanding = () => {
                 />
 
                 <div className={styles.menu} style={ menuOpen ? { "left": 0 } : { "left": "-100%" } }>
-                    <ScrollLink className={styles.menuItem} to="inicio" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenuOpen}>
-                        Inicio
-                    </ScrollLink>
-                    <ScrollLink className={styles.menuItem} to="que" spy={true} smooth={true} offset={-80} duration={500} onClick={closeMenuOpen}>
-                    ¿Qué hacemos?
-                    </ScrollLink>
-                    <ScrollLink className={styles.menuItem} to="test" spy={true} smooth={true} offset={-80} duration={500} onClick={closeMenuOpen}>
-                        Testimonios
-                    </ScrollLink>
-                    <Link to='demo' className={styles.verTutor}>
-                        Ver tutores
-                    </Link>
-                    <Link to='login' className={styles.login}>
+                    {
+                        demoMode ? (
+                            <>
+                                <Link to='/' className={styles.menuItem}>
+                                    Home
+                                </Link>
+                                <Link to='/register' className={styles.menuItem}>
+                                    ¡Ser tutor!
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <ScrollLink className={styles.menuItem} to="inicio" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenuOpen}>
+                                    Inicio
+                                </ScrollLink>
+                                <ScrollLink className={styles.menuItem} to="que" spy={true} smooth={true} offset={-80} duration={500} onClick={closeMenuOpen}>
+                                    ¿Qué hacemos?
+                                </ScrollLink>
+                                <ScrollLink className={styles.menuItem} to="test" spy={true} smooth={true} offset={-80} duration={500} onClick={closeMenuOpen}>
+                                    Testimonios
+                                </ScrollLink>
+                                <Link to='/demo' className={styles.verTutor}>
+                                    Ver tutores
+                                </Link>
+                            </>
+                        )
+                    }
+                    <Link to='/login' className={styles.login}>
                         Login
                     </Link>
-                    <Link to='register' className={styles.signUp}>
+                    <Link to='/register' className={styles.signUp}>
                         Sign up
                     </Link>
                 </div>
