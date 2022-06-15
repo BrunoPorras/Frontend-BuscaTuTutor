ae9de51948bd4c629f3fee61c9f386be561360e8
BrunoPorras <alefran2020@gmail.com>
2022-06-15 10:30:52 -0500

    Header dinámico

--- src/components/Login/Header.js
+++ src/components/Login/Header.js
@@ -1,10 +1,45 @@
-import styles from '../../styles/Navbar.module.css'
-import logo from '../../assets/Landing page/logo.png'
+//  React
+import { Link } from 'react-router-dom'
 
-const Header = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import styles from '../../styles/Navbar.module.css'
+import icono from '../../assets/Landing page/icono.png'
+import { useMenuMobile } from '../../hooks/useMenuMobile'
+
+const Header = ({ login = true }) => {
+
+    const [menuOpen, changeMenuOpen] = useMenuMobile()
+
     return(
-        <div className={styles.logo}>
-            <img src={logo} alt="Imagen" />
+        <div className={styles.container}>
+            <div className={styles.wraper}>
+                <div className={styles.logoContainer}>
+                    <img src={icono} alt="ícono de la app" />
+                </div>
+
+                <FontAwesomeIcon icon={faBars} 
+                    className={styles.toggle}
+                    onClick={changeMenuOpen}
+                />
+
+                <div className={styles.menu} style={ menuOpen ? { "left": 0 } : { "left": "-100%" } }>
+                    <Link to={-1} className={styles.menuItem}>
+                        Regresar
+                    </Link>
+                    {
+                        login ? (
+                            <Link to='/register' className={styles.signUp}>
+                                Sign up
+                            </Link>
+                        ) : (
+                            <Link to='/login' className={styles.login}>
+                                Login
+                            </Link>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
