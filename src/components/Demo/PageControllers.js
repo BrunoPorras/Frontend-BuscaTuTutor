699fdffc74352a5978475dd75499f429c9fb6dcd
BrunoPorras <alefran2020@gmail.com>
2022-07-16 00:56:31 -0500

    Controlador de paginación actualizado

--- src/components/Demo/PageControllers.js
+++ src/components/Demo/PageControllers.js
@@ -9,10 +9,10 @@ const PageControllers = ({ prevPage, nextPage, totalPages, currentPage }) => {
     return(
         <div className={styles.navigation}>
             <FontAwesomeIcon className={styles.changePage}
-            onClick={() => prevPage()} icon={faAnglesLeft}/>
+                onClick={() => prevPage()} icon={faAnglesLeft}/>
             <p style={{margin: "0 10px"}}>{currentPage} de {totalPages}</p>
             <FontAwesomeIcon className={styles.changePage}
-            onClick={() => nextPage(totalPages)} icon={faAnglesRight}/>
+                onClick={() => nextPage(totalPages)} icon={faAnglesRight}/>
         </div>
     )
 }
